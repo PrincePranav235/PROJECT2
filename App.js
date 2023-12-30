fd2a2862e969585c5ef492309f7abf82a78fa4a1
Prince Pranav <princep.ug20.cse@nitp.ac.in>
2023-12-30 16:58:26 +0530

    do

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import  ReactDOM,{createRoot}  from "react-dom/client";
 
+const a=<h1>Namaste react</h1>;
+const b= () =>{
+    return(
+        <div>
+            <h1>hehe ye h function comp asli ghuda diyr isko</h1>
+        </div>
+    );
+}
+const fn =()=>(
+   
+        <div>
+            {a}
+            {b()}
+            <h1>ye h heading from function component</h1>
+            <h2>transitive composition krdiyr h mtlb ghusa diye h component ko component mein</h2>;
+        </div>
+);
 
-const h2=React.createElement("h1",{},"yr rha heading");
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(h2);
+root.render(fn());
